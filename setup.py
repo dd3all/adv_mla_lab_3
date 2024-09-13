cfcb7dc22972ec789c5b17862f39848e42831119
dd3all <dd3al@hotmail.com>
2024-09-13 15:46:53 +1000

    project init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='lab_3',
+    author='Diliyaer',
+    license='MIT',
+)
